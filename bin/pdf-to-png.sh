f7bc63cb1cc6119baa1c27577a8b22d693dec474
Vincent van Bergen <v.vanbergen@gmail.com>
2019-01-29 11:24:56 +0100

    update

--- bin/pdf-to-png.sh
+++ bin/pdf-to-png.sh
@@ -2,7 +2,7 @@
 
 file="$1"
 
-for file in /home/vincent/Apps/tuscany/plattegronden/**/*.pdf
-do
+# for file in /home/vincent/Apps/tuscany/plattegronden/**/*.pdf
+# do
 gs -q -sDEVICE=png16m -dSubsetFonts=true -dEmbedAllFonts=true -sOutputFile="$file"-%d.png -r200 -dBATCH -dNOPAUSE "$file"
-done
+# done
